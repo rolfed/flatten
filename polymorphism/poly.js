d2da9fc63d8804478b7ff34f1767cc870290d7a7
Danniel Rolfe <danniel.rolfe@standard.com>
2017-09-18 16:34:12 -0700

    COOL

--- polymorphism/poly.js
+++ polymorphism/poly.js
@@ -35,17 +35,82 @@ var denise = new Female('Denise');
 
 // female.prototype.cityOfBirth = 'San Antonio';
 
-console.log('Male: ', Male.prototype);
+// console.log('Male: ', Male.prototype);
 
-danniel.cityOfBirth = 'Sao Paulo';
-danniel.sayCityOfBirth();
-danniel.sayGender();
+// danniel.cityOfBirth = 'Sao Paulo';
+// danniel.sayCityOfBirth();
+// danniel.sayGender();
 
-console.log('Danniel: ', danniel);
+// console.log('Danniel: ', danniel);
 
 
-denise.cityOfBirth = 'San Antonio';
-denise.sayCityOfBirth();
-denise.sayGender();
+// denise.cityOfBirth = 'San Antonio';
+// denise.sayCityOfBirth();
+// denise.sayGender();
+
+
+/*
+ * Car example
+ */
+
+var Automobile = function(){};
+
+Automobile.prototype = {
+    model: '',
+    make: '',
+    year: null,
+    color: '',
+    type: '',
+    miles: null,
+    gallons: null,
+    calcMpg: function() {
+        console.log('A ' + this.year + ' ' + this.make + ' ' + this.model + ' gets ' + (this.miles/this.gallons) + ' miles per gallon');
+    },
+};
+
+function SUV(model, make){
+    this.model = model;
+    this.make = make;
+    this.doors = 4;
+    this.fourWheelDrive = true;
+}
+
+function Truck(model, make){
+    this.model = model;
+    this.make = make;
+    this.doors = 2;
+    this.fourWheelDrive = true;
+}
+
+function Van(model, make){
+    this.model = model;
+    this.make = make;
+    this.doors = 6;
+    this.fourWheelDrive = false;
+}
+
+extendObj(SUV, Automobile);
+extendObj(Truck, Automobile);
+extendObj(Van, Automobile);
+
+var pilot = new SUV('Honda', 'Pilot');
+pilot.year = 2017;
+pilot.color = "green";
+pilot.type = 'SUV';
+pilot.miles = 20;
+pilot.gallons = 60;
+
+console.log('Pilot: ', pilot);
+pilot.calcMpg();
+
+var sprinter = new Van('Mercedes', 'Sprinter');
+sprinter.year = 2014;
+sprinter.color = "silver";
+sprinter.type = 'Van';
+sprinter.miles = 18;
+sprinter.gallons = 400;
+
+console.log('\n\nSprinter: ', sprinter);
+sprinter.calcMpg();
 
 

aeb25e46804181a48c36541cb6f95128b85b5bf4
Danniel Rolfe <danniel.rolfe@standard.com>
2017-09-28 21:22:10 -0700

    What

--- merge-and-sort/mergeAndSortTwo.js
+++ merge-and-sort/mergeAndSortTwo.js
@@ -2,10 +2,11 @@
 
 var mergeAndSort = (function(){
 
-    var arr = [0,8,3,6,3,'87','32','19'];
+    const arr = [0,8,3,6,3,'87','32','19'];
+    console.log('Initial arr: ', arr);
 
     function checkIfString (arr) {
-        var stringArr = arr.filter(function(value) {
+        const stringArr = arr.filter(function(value) {
             return (typeof value === 'string')
         })
         .map(function(string) {
@@ -16,7 +17,7 @@ var mergeAndSort = (function(){
     }
 
     function checkIfInteger(arr){
-        var integerArr = arr.filter(function(value) {
+        const integerArr = arr.filter(function(value) {
             return (typeof value === 'number');
         })
         .map(function(integer) {
@@ -30,12 +31,46 @@ var mergeAndSort = (function(){
         return checkIfInteger(arr).concat(checkIfString(arr));
     }
 
-    function mergeAndSort(arr) {
-        console.log('Arr: ', arr);
+    function mergeSort(arr) {
+        combineArr(arr);
+
+        if (arr.length === 1) {
+            return arr;
+        }
+
+        const middle = Math.floor(arr.length / 2);
+        const left = arr.slice(0, middle);
+        const right = arr.slice(middle);
+
+        return merge(
+                mergeSort(left),
+                mergeSort(right)
+        )
+    }
+
+    function merge (left, right) {
+        let result = [];
+        let indexLeft = 0;
+        let indexRight = 0;
+
+        while (indexLeft < left.length && indexRight < right.length) {
+            if (left[indexLeft] < right[indexRight]) {
+                result.push(left[indexLeft]);
+                indexLeft++
+            }
+
+            else {
+                result.push(right[indexRight])
+                indexRight++
+            }
+        }
+
+        return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+
     }
 
     function init() {
-        mergeAndSort(combineArr(arr));
+        console.log('Arr: ', combineArr(mergeSort(arr)));
     }
 
     return {
